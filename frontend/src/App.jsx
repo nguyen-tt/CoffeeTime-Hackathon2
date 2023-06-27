8ff62856c3a53a0a75c7fa30b8b081e393481187
Trung Nguyen <[trung.nguyenpro@hotmail.com](mailto:trung.nguyenpro@hotmail.com)>
2023-06-27 22:17:54 +0200

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Page1 from "@pages/Page1";
 import Page2 from "@pages/Page2";
 import Contact from "@pages/Contact";
 import Footer from "@components/Footer";
+import NotFound from "@pages/NotFound";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 
@@ -19,6 +20,7 @@ function App() {
           <Route path="/page1" element={<Page1 />} />
           <Route path="/page2" element={<Page2 />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
         <Footer />
       </Router>
