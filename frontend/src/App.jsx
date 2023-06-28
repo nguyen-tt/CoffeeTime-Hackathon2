c3abb71d8b49112c7b8f522acd0ec0e54d6fbf79
Anne LOUIS <anne.louis51100@gmail.com>
2023-06-28 19:08:20 +0200

    hash password

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,38 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import Page1 from "@pages/Page1";
-import Page2 from "@pages/Page2";
-import Contact from "@pages/Contact";
-import Footer from "@components/Footer";
-import NotFound from "@pages/NotFound";
-import Header from "./components/Header";
+import Page1 from "./pages/Page1";
+import Page2 from "./pages/Page2";
+import Contact from "./pages/Contact";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import NotFound from "./pages/NotFound";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+import { CurrentUserProvider } from "./contexts/CurrentUser";
 
 import "./styles.scss";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/page1" element={<Page1 />} />
-          <Route path="/page2" element={<Page2 />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-        <Footer />
-      </Router>
+      <CurrentUserProvider>
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/page1" element={<Page1 />} />
+            <Route path="/page2" element={<Page2 />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<NotFound />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </CurrentUserProvider>
     </div>
   );
 }
