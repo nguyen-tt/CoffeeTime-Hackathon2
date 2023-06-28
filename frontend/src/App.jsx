62704549cb26ae9f67d097189373285a3492c84d
Trung Nguyen <[trung.nguyenpro@hotmail.com](mailto:trung.nguyenpro@hotmail.com)>
2023-06-28 22:23:30 +0200

    fix dev

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,10 +9,6 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
-import NotFound from "./pages/NotFound";
-
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 
 import { CurrentUserProvider } from "./contexts/CurrentUser";
 
