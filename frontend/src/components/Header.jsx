da0fde40e3790bb96e6477ab8640d1dbb5d0ab6a
Trung Nguyen <[trung.nguyenpro@hotmail.com](mailto:trung.nguyenpro@hotmail.com)>
2023-06-28 20:44:03 +0200

    change navbar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,39 +1,40 @@
 import { NavLink, useLocation } from "react-router-dom";
 
+import logo from "@assets/logo.svg";
+
 export default function Header() {
   const location = useLocation();
 
   return (
     <header className="header">
-      <div className="logo">LOGO</div>
-      <nav className="navbar">
-        <ul>
-          <NavLink
-            to="/"
-            className={location === "/page1" ? "navlink active" : "navlink"}
-          >
-            <li>Home</li>
-          </NavLink>
-          <NavLink
-            to="/page1"
-            className={location === "/page1" ? "navlink active" : "navlink"}
-          >
-            <li>Page 1</li>
-          </NavLink>
-          <NavLink
-            to="/page2"
-            className={location === "/page2" ? "navlink active" : "navlink"}
-          >
-            <li>Page 2</li>
-          </NavLink>
-          <NavLink
-            to="/contact"
-            className={location === "/contact" ? "navlink active" : "navlink"}
-          >
-            <li>Contact</li>
-          </NavLink>
-        </ul>
-      </nav>
+      <div className="top-header" />
+      <div className="bottom-header">
+        <NavLink to="/">
+          <img src={logo} alt="logo" className="logo" />
+        </NavLink>
+        <nav className="navbar">
+          <ul>
+            <NavLink
+              to="/add"
+              className={location === "/add" ? "navlink active" : "navlink"}
+            >
+              <li>Ajout</li>
+            </NavLink>
+            <NavLink
+              to="/data"
+              className={location === "/data" ? "navlink active" : "navlink"}
+            >
+              <li>Base de données</li>
+            </NavLink>
+            <NavLink
+              to="/faq"
+              className={location === "/faq" ? "navlink active" : "navlink"}
+            >
+              <li>FAQ</li>
+            </NavLink>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
