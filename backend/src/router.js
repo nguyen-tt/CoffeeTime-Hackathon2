7a54c0ff7c838a6f75bd2383066cb30bd3a4caaa
MarineDeveza <marine.deveza@gmail.com>
2023-06-28 16:06:36 +0200

    routesusers

--- backend/src/router.js
+++ backend/src/router.js
@@ -3,11 +3,16 @@ const express = require("express");
 const router = express.Router();
 
 const itemControllers = require("./controllers/itemControllers");
+const usersControllers = require("./controllers/usersControllers");
 
 router.get("/items", itemControllers.browse);
 router.get("/items/:id", itemControllers.read);
 router.put("/items/:id", itemControllers.edit);
 router.post("/items", itemControllers.add);
 router.delete("/items/:id", itemControllers.destroy);
+
+router.get("/users", usersControllers.browse);
+router.get("/users/:id", usersControllers.read);
+router.delete("/users/:id", usersControllers.destroy);
 
 module.exports = router;
