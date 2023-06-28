60cbffe5dd32b12943c9a0158c59f6838c308045
Trung Nguyen <[trung.nguyenpro@hotmail.com](mailto:trung.nguyenpro@hotmail.com)>
2023-06-28 18:43:20 +0200

    added BDD

--- backend/database.sql
+++ backend/database.sql
@@ -1,3 +1,35 @@
+CREATE TABLE screen_sizes (
+    id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    size VARCHAR(255) NOT NULL,
+    size_value VARCHAR(255) NOT NULL
+) ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+INSERT INTO screen_sizes (size, size_value) VALUES ('6','40'),('7','44'),('8','49'),('9','53'); /* A VERIFIER LES VALEURS */
+
+CREATE TABLE ram_memories (
+    id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    memory_number VARCHAR(255) NOT NULL,
+    memory_value VARCHAR(255) NOT NULL
+) ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+INSERT INTO ram_memories (memory_number, memory_value) VALUES ('1','30'),('2','40'),('3','50'),('4','60'),('6','70'),('8','80'),('12','90'); /* A VERIFIER LES VALEURS */
+
+CREATE TABLE storage_values (
+    id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    number_storage VARCHAR(255) NOT NULL,
+    storage_value VARCHAR(255) NOT NULL
+) ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+INSERT INTO storage_values (number_storage,storage_value) VALUES ('16','31'),('32','45'),('65','66'); /* AJOUTER VALUE */
+
+CREATE TABLE states (
+    id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    state VARCHAR(255) NOT NULL,
+    weighting VARCHAR(255) NOT NULL
+) ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+INSERT INTO states (state, weighting) VALUES ('Parfait', '0'), ('Très bon', '0.90'), ('Correct','0.8');
+
 CREATE TABLE smartphones (
     id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     brand VARCHAR(255) NOT NULL,
@@ -6,53 +38,22 @@ CREATE TABLE smartphones (
     storage VARCHAR(255) NOT NULL,
     screen VARCHAR(255) NOT NULL,
     network VARCHAR(255) NOT NULL,
-    loader TINYINT(1) NOT NULL,
+    loader TINYINT(1) NOT NULL DEFAULT 1,
     operating_system VARCHAR(255) NOT NULL,
     operating_system_version VARCHAR(255) NOT NULL,
     images VARCHAR(255) NOT NULL,
-    screen_size_id, FOREIGN KEY (screen_size_id) REFERENCES screen_sizes(id),
-    ram_memory_id, FOREIGN KEY (ram_memory_id) REFERENCES ram_memories(id),
-    storage_value_id, FOREIGN KEY (storage_value_id) REFERENCES storage_values(id),
-    state_id, FOREIGN KEY (state_id) REFERENCES states(id)
+    screen_size_id INT UNSIGNED, FOREIGN KEY (screen_size_id) REFERENCES screen_sizes(id),
+    ram_memory_id INT UNSIGNED, FOREIGN KEY (ram_memory_id) REFERENCES ram_memories(id),
+    storage_value_id INT UNSIGNED, FOREIGN KEY (storage_value_id) REFERENCES storage_values(id),
+    state_id INT UNSIGNED, FOREIGN KEY (state_id) REFERENCES states(id)
 )  ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
-INSERT INTO smartphones (brand) VALUES ("Iphone", "Samsung", "Google");
-INSERT INTO smartphones (model) VALUES ("PRO MAX", "A", "Pixel 7a"); /* A redéfinir */
-INSERT INTO smartphones (network) VALUES ("3G", "4G", "5G");
-INSERT INTO smartphones (operating_system) VALUES ("IOS", "Android");
-INSERT INTO smartphones (operating_system_version) VALUES ("10", "11","12","13","14","15","16");
+INSERT INTO smartphones (brand, model, ram, storage, screen, network, operating_system, operating_system_version, images)
+VALUES ('Apple', 'Iphone 12 Pro','6','256','6', '5G', 'IOS', '16','iphone-12pro.jpg'),
+       ('Samsung', 'Galaxy S22 Ultra','12','256','6', '5G', 'Android', '13','samsung-galaxys22ultra.jpg'),
+       ('Google', 'Pixel 6 Pro','12','256','6', '5G', 'Android', '13','google-pixel6pro.jpg');
 
-CREATE TABLE screen_sizes (
-    id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    size VARCHAR(255) NOT NULL,
-    size_value VARCHAR(255) NOT NULL
-) ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
-INSERT INTO screen_sizes (size) VALUES ("6","7","8","9"); /* AJOUTER VALUE */
-
-CREATE TABLE ram_memories (
-    id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    memory_number VARCHAR(255) NOT NULL,
-    memory_value VARCHAR(255) NOT NULL
-) ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-
-INSERT INTO ram_memories (memory_number) VALUES ("1","2","3","4","6","8","12"); /* AJOUTER VALUE */
-
-CREATE TABLE storage_values (
-    id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    number_storage VARCHAR(255) NOT NULL,
-    storage_value VARCHAR(255) NOT NULL
-) ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-
-INSERT INTO storage_values (number_storage) VALUES ("16","32","65","128","256","512","1000"); /* AJOUTER VALUE */
-
-CREATE TABLE states (
-    id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    state VARCHAR(255) NOT NULL,
-    weighting VARCHAR(255) NOT NULL
-) ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-
-INSERT INTO states (state, weighting) VALUES ("Parfait", "0"), ("Très bon", "0.90"), ("Correct","0.8");
 
 CREATE TABLE users (
   id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
